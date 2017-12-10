cb97f1945a9a2e0bd89a7ff2061608d43b8ecee9
Ryan OKeefe <ryanokeefe208@gmail.com>
2017-12-10 08:59:11 -0500

    final

--- new.js
+++ new.js
@@ -9,6 +9,13 @@ var depositChecking = buttons[1]
 var withdrawChecking = buttons[2]
 var accountDisplayChecking = document.getElementsByClassName('balance')[0]
 
+// Savings vars:
+
+var depositSavings = buttons[4]
+var withdrawSavings = buttons[5]
+var inputSavings = document.getElementById('savings-input')
+accountDisplaySavings = document.getElementsByClassName('balance')[1]
+
 /////////////////////
 
 // Checking Account Balance:
@@ -37,10 +44,11 @@ depositChecking.addEventListener('click', function () {
 
 })
 
-// Withdraw Event Listener
+// checking Withdraw Event Listener
 
 withdrawChecking.addEventListener("click", 
 function () {
+  
     // Get Checking input value: 
   var withdrawValueChecking = parseInt(inputChecking.value)
   console.log(withdrawValueChecking)
@@ -48,16 +56,56 @@ function () {
   // split and convert to integer
   var initialAccValue = parseInt(accountDisplayChecking.innerHTML.split('$')[1])
 
-  // Combine deposit with balance
-  var newBalance = '$' + (initialAccValue - withdrawValueChecking)
+  // IF available
+  if (initialAccValue - withdrawValueChecking >= 0) {
+    // Take withdraw from balance 
+    var newBalance = '$' + (initialAccValue - withdrawValueChecking)
+    console.log(newBalance)
+
+    // Insert New Balance into HTML
+    accountDisplayChecking.innerHTML = newBalance
+  }
+})
+  
+// Deposit Savings Event Listener
+
+
+depositSavings.addEventListener('click', function () {
+  // Get Savings input value: 
+var depositValueSavings = parseInt(inputSavings.value)
+console.log(depositValueSavings)
+
+// split and convert to integer
+var initialAccValue = parseInt(accountDisplaySavings.innerHTML.split('$')[1])
+
+// Combine deposit with balance
+var newBalance = '$' + (initialAccValue + depositValueSavings)
+console.log(newBalance)
+
+// Insert New Balance into HTML
+accountDisplaySavings.innerHTML = newBalance
+
+})
+
+// Withdraw Savings Event Listener:
+
+
+withdrawSavings.addEventListener('click', function () {
+  // Get savings input value: 
+var withdrawValueSavings = parseInt(inputSavings.value)
+console.log(withdrawValueSavings)
+
+// split and convert to integer
+var initialAccValue = parseInt(accountDisplaySavings.innerHTML.split('$')[1])
+
+// IF available
+  if (initialAccValue - withdrawValueSavings >= 0) {
+  // Take withraw from balance
+  var newBalance = '$' + (initialAccValue - withdrawValueSavings)
   console.log(newBalance)
 
   // Insert New Balance into HTML
-  accountDisplayChecking.innerHTML = newBalance
-
+  accountDisplaySavings.innerHTML = newBalance
+  }
 })
-  
-
-
-
 
